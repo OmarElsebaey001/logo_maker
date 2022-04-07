47a1b6519a0d7147f2556942ce4f1611071cfdcf
omarelsebaey001 <omar.cufe@gmail.com>
2022-04-08 01:16:47 +0200

    Better implementation

--- app.py
+++ app.py
@@ -26,14 +26,19 @@ def create_app() :
             lg_bk_color = request.args.get("back_ground_color",type=str)
             lg_font_file = request.args.get("font_file",type=str)
             lg_font_size = request.args.get("font_size",type=int)
+            
             lg_txt_color = lg_txt_color.strip()
             lg_bk_color = lg_bk_color.strip()
             
             generate_image_simple_mode(lg_width,lg_height,lg_margin,lg_text,
-            lg_txt_color,lg_bk_color,lg_font_file,lg_font_size) 
-            if ( lg_text == None) : 
-                return render_template("index.html")
-            return render_template("processed.html")
+            lg_txt_color,lg_bk_color,lg_font_file,lg_font_size)
+
+            return render_template("processed.html",
+            logo_width=lg_width,logo_height=lg_height,logo_text=lg_text,
+            logo_margin=lg_margin,text_color=lg_txt_color,
+            back_ground_color=lg_bk_color,font_file=lg_font_file,
+            font_size=lg_font_size,
+            )
         return render_template("index.html")
     if __name__ == "__main__":
         app.run()
