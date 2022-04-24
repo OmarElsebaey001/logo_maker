c30020b72c83e4dd294fc7c299090520466328c1
omarelsebaey001 <omar.cufe@gmail.com>
2022-04-24 21:42:32 +0200

    one liner

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from operator import methodcaller
+from tkinter import font
 from flask import make_response,Flask,render_template
 from flask import request, jsonify
 from PIL import Image
@@ -6,7 +7,7 @@ from PIL import ImageFont
 from PIL import ImageDraw 
 from jinja2 import Template
 from flask import send_file
-from helper import generate_image_simple_mode
+from helper import *
 import io 
 from random import randint
 
@@ -18,28 +19,38 @@ def create_app() :
             lg_text = request.args.get("logo_text")
             if ( lg_text == None) : 
                 return render_template("index.html")
-            lg_width  = request.args.get("logo_width",type=int)
-            lg_height = request.args.get("logo_height",type=int)
-            lg_margin = request.args.get("logo_margin",type=int)
-            lg_text = request.args.get("logo_text",type=str)
+            lg_width     = request.args.get("logo_width",type=int)
+            lg_height    = request.args.get("logo_height",type=int)
+            lg_margin    = request.args.get("logo_margin",type=int)
+            lg_text      = request.args.get("logo_text",type=str)
             lg_txt_color = request.args.get("text_color",type=str)
-            lg_bk_color = request.args.get("back_ground_color",type=str)
+            lg_bk_color  = request.args.get("back_ground_color",type=str)
             lg_font_file = request.args.get("font_file",type=str)
-            lg_font_size = request.args.get("font_size",type=int)
             
             lg_txt_color = lg_txt_color.strip()
-            lg_bk_color = lg_bk_color.strip()
+            lg_text      = lg_text.strip()
+            lg_bk_color  = lg_bk_color.strip()
             
-            generate_image_simple_mode(lg_width,lg_height,lg_margin,lg_text,
-            lg_txt_color,lg_bk_color,lg_font_file,lg_font_size)
+            usable_per = (100-lg_margin)/100
+            
+            usable_w   = usable_per*lg_width
+            usable_h   = usable_per*lg_height
+            print("Usable: H" , usable_h)
+            print("Usable: W" , usable_w)
+            
+            font_file  = lg_font_file
+            font_size  = get_max_font(usable_w,usable_h,font_file,lg_text)
+            print("Max Font size: " , font_size)
+            img = generate_image_simple_mode(lg_width,lg_height,lg_text,lg_txt_color,lg_bk_color,font_file,font_size)
+            print(img.size)
+            img.save("static/processed_image.png")
 
             return render_template("processed.html",
             logo_width=lg_width,logo_height=lg_height,logo_text=lg_text,
             logo_margin=lg_margin,text_color=lg_txt_color,
-            back_ground_color=lg_bk_color,font_file=lg_font_file,
-            font_size=lg_font_size,
-            )
+            back_ground_color=lg_bk_color,font_file=lg_font_file)
+
         return render_template("index.html")
     if __name__ == "__main__":
         app.run()
-    return app 
+    return app
--- helper.py
+++ helper.py
@@ -1,37 +1,19 @@
 from PIL import Image, ImageDraw, ImageFont
-def generate_image_simple_mode(w,h,margin,txt,txt_color,bkground_color,font_file,size):
-    print("Recieved: ", w,h,margin,txt,txt_color,bkground_color,font_file,size)
-    w_margin_percent = margin
-    h_margin_percent = margin
-
-    w_margin = int(w * ( (w_margin_percent/100) ))
-    h_margin = int(h * ( (h_margin_percent/100) ))
-
-    w_margined_size = w - w_margin
-    h_margined_size = h - h_margin
-
-    shape = [(w_margin/2, h_margin/2), (w - w_margin/2, h - h_margin/2 )]
-
-    #print("New width and hight : " , w_margined_size,h_margined_size)
-
-    #txt = "Center New"
-    #txt_color = "white"
-
+def generate_image_simple_mode(w,h,txt,txt_color,bkground_color,font_file,size):
     font = ImageFont.truetype(font_file, size)
-    #pot_txt_w, pot_txt_h = font.getsize(txt)
-    #print("Potential size of text : " , pot_txt_w, pot_txt_h)
-    #print("Available size : " , (w_margined_size,h_margined_size))
-    #if(pot_txt_w > w_margined_size ) : 
-    #    print("Width is too large")
-    #else : 
-    #    print("Width fits")
-
-    #if(pot_txt_h > h_margined_size ) : 
-    #    print("Heigh is too large")
-    #else : 
-    #    print("Height fits")
     im = Image.new("RGB", (w,h), bkground_color)
-    d = ImageDraw.Draw(im)
+    d  = ImageDraw.Draw(im)
     d.text((w/2, h/2), txt , fill=txt_color, font=font, anchor="mm")
-    #d.rectangle(shape, fill=(0, 0,0), outline=(255, 255, 255))
-    im.save("static/processed_image.png","PNG")
+    return im 
+def get_max_font(w,h,font_file,text):
+    usable_font = 0 
+    i = 1 
+    while(True):
+        font = ImageFont.truetype(font_file, i)
+        text_size = font.getsize(text)
+        if (text_size[0] < w and text_size[1] < h ) : 
+            usable_font = i 
+        else:
+            break 
+        i += 1 
+    return usable_font 
