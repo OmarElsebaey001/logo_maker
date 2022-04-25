d9a87f855e4eae134509d052b57fbdb4ebaa74fd
omarelsebaey001 <omar.cufe@gmail.com>
2022-04-25 21:00:14 +0200

    Adding the full image creator

--- script_helper.py
+++ script_helper.py
@@ -1,33 +1,54 @@
 from PIL import Image, ImageDraw, ImageFont
-import pandas as pd 
-import sys
 
-def get_max_font(w,h,font_file,text):
+
+def get_max_font(w, h, font_file, text):
     usable_font = 0 
-    i = 1 
-    while(True):
+    i = 1
+    while True:
         font = ImageFont.truetype(font_file, i)
-        text_size = font.getsize(text)
-        if (text_size[0] < w and text_size[1] < h ) : 
+        text_size = get_text_size_from_font(text,font)
+        if (text_size[0] < w) and (text_size[1] < h):
             usable_font = i 
         else:
+            print("Breaking with size: ", text_size)
+            print("Breaking with font: ", usable_font)
             break 
         i += 1 
     return usable_font 
 
 
-def generate_image_simple_mode(w,h,txt,txt_color,bkground_color,font_file,size):
+def generate_image_simple_mode(w, h, txt, txt_color, background_color, font_file, size):
+    print("Writing in usable: " , w , h )
     font = ImageFont.truetype(font_file, size)
-    im = Image.new("RGB", (w,h), bkground_color)
-    d  = ImageDraw.Draw(im)
-    d.text((w/2, h/2), txt , fill=txt_color, font=font, anchor="mm")
+    t_h = font.getsize(txt)[1]
+    im = Image.new("RGB", (w, h), background_color)
+    d = ImageDraw.Draw(im)
+    w_shift = w/2 
+    h_shift = (h)/2
+    #h_shift = (h+t_h/4)/2
+    
+    print("Shifting: " , w_shift)
+    print("Shifting: " , h_shift)
+    d.text((w_shift, h_shift), txt , fill=txt_color, font=font, anchor="mm")
     return im 
 
 
-def create_full_image(lg_width,lg_height,margin,lg_text,font_file,lg_txt_color,lg_bk_color):
+def create_full_image(lg_width, lg_height, margin, lg_text, font_file, lg_txt_color, lg_bk_color):
+    lg_text = lg_text.replace("<br>","\n")
     usable_per = (100-margin)/100
-    usable_w   = usable_per*lg_width
-    usable_h   = usable_per*lg_height
-    font_size  = get_max_font(usable_w,usable_h,font_file,lg_text)
-    img = generate_image_simple_mode(lg_width,lg_height,lg_text,lg_txt_color,lg_bk_color,font_file,font_size)
+    usable_w = int(usable_per*lg_width)
+    usable_h = int(usable_per*lg_height)
+    font_size = get_max_font(usable_w, usable_h, font_file, lg_text)
+    img = generate_image_simple_mode(lg_width, lg_height, lg_text, lg_txt_color, lg_bk_color, font_file, font_size)
     return img
+
+
+def get_text_size_from_font(text,fnt):
+
+    im = Image.new(mode="RGB", size=(1,1))
+    draw = ImageDraw.Draw(im)
+    if("\n" in text):
+        size = draw.multiline_textsize(text, font=fnt)
+    else:
+        size = draw.textsize(text, font=fnt)
+    return size
