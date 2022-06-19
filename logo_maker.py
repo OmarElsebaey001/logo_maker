e54a7107aba97cd385a675168ef16cf21da97455
omarelsebaey001 <omar.cufe@gmail.com>
2022-06-19 16:48:28 +0000

    adding the new changes

--- logo_maker.py
+++ logo_maker.py
@@ -1,5 +1,6 @@
 from script_helper import * 
-
+import sys 
+import pandas as pd 
 input_file = sys.argv[1]
 main_df = pd.read_csv(input_file)
 for i in range(main_df.shape[0]):
@@ -10,6 +11,7 @@ for i in range(main_df.shape[0]):
     f     = main_df['font_file'][i]
     bc    = main_df['back_color'][i]
     fc    = main_df['front_color'][i]
+    bord  = main_df['border'][i]
     o     = main_df['output_name'][i] +'.png' 
-    image = create_full_image(w,h,m,t,f,bc,fc)
+    image = create_full_image(w,h,m,t,f,bc,fc,bord)
     image.save(o)
